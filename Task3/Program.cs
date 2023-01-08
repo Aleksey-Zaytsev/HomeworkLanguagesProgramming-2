ddfe76f9c2bc0aa94fc4c41fa750d6effb821224
Aleksey <a.y.zayitsev@gmail.com>
2023-01-08 22:02:48 +0300

    Пргорамма показывающая дни недели 1 вариант

---
+++ Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+Console.WriteLine ("Введите цифру обозначающую день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7)
+{
+    Console.WriteLine ("Введите число от 1 до 7");
+}
+
+if (number < 1 )
+{
+    Console.WriteLine ("Введите число от 1 до 7");
+}
+if (number == 6) 
+{
+    Console.WriteLine ("Это Суббота - Выходной день!");
+}
+if (number == 7) 
+{
+    Console.WriteLine ("Это Воскресенье - Выходной день!");
+}
+if (number == 1) 
+{
+    Console.WriteLine ("Это Понедельник - Рабочий день!");
+}
+if (number == 2) 
+{
+    Console.WriteLine ("Это Вторник - Рабочий день!");
+}
+if (number == 3) 
+{
+    Console.WriteLine ("Это Среда - Рабочий день!");
+}
+if (number == 4) 
+{
+    Console.WriteLine ("Это Четверг - Рабочий день!");
+}
+if (number == 5) 
+{
+    Console.WriteLine ("Это Пятница - Рабочий день!");
+}
