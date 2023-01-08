007bcb963a1e48c075aedf4d171e97b10f6f02b4
Aleksey <a.y.zayitsev@gmail.com>
2023-01-08 17:30:38 +0300

    Программа для вывода второй цифры из трехзначного числа

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Write ("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number > 99) && (number < 1000))
+{
+  int result = number / 10;
+  int result2 = result % 10;
+  Console.WriteLine (result2);
+}
+else
+{
+    Console.WriteLine ("Это число не трехзначное");
+}
+
